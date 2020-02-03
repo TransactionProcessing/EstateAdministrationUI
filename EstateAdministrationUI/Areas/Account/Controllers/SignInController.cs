df9404e79e1ec72b6d38db4cd2ce56e5394f36b2
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-02-03 19:46:39 +0000

    Skelton Application

---
+++ EstateAdministrationUI/Areas/Account/Controllers/SignInController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfClubAdminWebSite.Areas.Account.Controllers
+{
+    using System.Threading;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    
+    [Area("Account")]
+    public class SignInController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Login(CancellationToken cancellationToken)
+        {
+            return this.RedirectToAction(nameof(this.LoggedIn));
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> LoggedIn(CancellationToken cancellationToken)
+        {
+            // Decide which page we move to now based on the role
+            return this.DetermineLoggedInView();
+        }
+
+        private IActionResult DetermineLoggedInView()
+        {
+            IActionResult actionResult = null;
+
+            if (this.User.IsInRole("Estate"))
+            {
+                actionResult = this.RedirectToAction("Index",
+                                                     "Home",
+                                                     new
+                                                     {
+                                                         //Area = "EstateAdministrator"
+                                                     });
+            }
+            else
+            {
+                // TODO: This should throw some kind of error as not supported
+            }
+
+            return actionResult;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task Logout(CancellationToken cancellationToken)
+        {
+            await this.HttpContext.SignOutAsync("oidc");
+            await this.HttpContext.SignOutAsync("Cookies");
+        }
+    }
+}
