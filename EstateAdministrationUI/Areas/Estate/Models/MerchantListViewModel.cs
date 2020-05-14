a94123aec1e915ef4587aca7c9186e1b39aeea76
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-05-14 15:09:23 +0100

    wip

---
+++ EstateAdministrationUI/Areas/Estate/Models/MerchantListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstateAdministrationUI.Areas.Estate.Models
+{
+    public class MerchantListViewModel
+    {
+    }
+}
