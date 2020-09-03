6a5f0155f303f5209db9a40cc99f9d4a72456f78
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-09-03 07:07:46 +0100

    log level trace

--- EstateAdministrationUI.IntegrationTests/Common/Hooks.cs
+++ EstateAdministrationUI.IntegrationTests/Common/Hooks.cs
@@ -32,8 +32,7 @@
             options.AddArguments("--headless");
             options.AcceptInsecureCertificates = true;
             options.UnhandledPromptBehavior = UnhandledPromptBehavior.Accept;
-            options.UseLegacyImplementation = true;
-            options.LogLevel = FirefoxDriverLogLevel.Debug;
+            options.LogLevel = FirefoxDriverLogLevel.Trace;
 
             this.WebDriver = new FirefoxDriver(options);
 
