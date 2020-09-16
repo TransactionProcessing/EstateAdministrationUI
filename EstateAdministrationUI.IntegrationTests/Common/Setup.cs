e27e024c4ceee0c9ba8f5c55ed0068bb998192d0
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-09-16 10:21:38 +0100

    :|

--- EstateAdministrationUI.IntegrationTests/Common/Setup.cs
+++ EstateAdministrationUI.IntegrationTests/Common/Setup.cs
@@ -1,14 +1,24 @@
 ﻿namespace EstateAdministrationUI.IntegrationTests.Common
 {
     using System;
+    using System.Data;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Reflection;
+    using System.Threading;
     using Ductus.FluentDocker;
     using Ductus.FluentDocker.Builders;
     using Ductus.FluentDocker.Services;
     using Ductus.FluentDocker.Services.Extensions;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Logging;
     using NLog;
+    using Shared.IntegrationTesting;
     using Shared.Logger;
     using Shouldly;
     using TechTalk.SpecFlow;
+    using ILogger = Microsoft.Extensions.Logging.ILogger;
 
     [Binding]
     public class Setup
@@ -50,14 +60,164 @@
             }
 
             // Start the Database Server here
-            Setup.DatabaseServerContainer = global::Shared.IntegrationTesting.DockerHelper.StartSqlContainerWithOpenConnection(Setup.SqlServerContainerName,
+            Setup.DatabaseServerContainer = Setup.StartSqlContainerWithOpenConnection(Setup.SqlServerContainerName,
                                                                                                                                logger,
                                                                                                                                sqlServerImageName,
                                                                                                                                Setup.DatabaseServerNetwork,
-                                                                                                                               "",
                                                                                                                                dockerCredentials,
                                                                                                                                Setup.SqlUserName,
                                                                                                                                Setup.SqlPassword);
+        }
+
+        /// <summary>
+        /// Starts the SQL container with open connection.
+        /// </summary>
+        /// <param name="containerName">Name of the container.</param>
+        /// <param name="logger">The logger.</param>
+        /// <param name="imageName">Name of the image.</param>
+        /// <param name="networkService">The network service.</param>
+        /// <param name="hostFolder">The host folder.</param>
+        /// <param name="dockerCredentials">The docker credentials.</param>
+        /// <param name="sqlUserName">Name of the SQL user.</param>
+        /// <param name="sqlPassword">The SQL password.</param>
+        /// <returns></returns>
+        public static IContainerService StartSqlContainerWithOpenConnection(String containerName,
+                                                                            NlogLogger logger,
+                                                                            String imageName,
+                                                                            INetworkService networkService,
+                                                                            (String URL, String UserName, String Password)? dockerCredentials,
+                                                                            String sqlUserName = "sa",
+                                                                            String sqlPassword = "thisisalongpassword123!")
+        {
+            logger.LogInformation("About to start SQL Server Container");
+            IContainerService databaseServerContainer = new Builder().UseContainer().WithName(containerName).UseImage(imageName)
+                                                                     .WithEnvironment("ACCEPT_EULA=Y", $"SA_PASSWORD={sqlPassword}").ExposePort(1433)
+                                                                     .UseNetwork(networkService).KeepContainer().KeepRunning().ReuseIfExists().Build().Start();
+
+            logger.LogInformation("SQL Server Container Started");
+
+            logger.LogInformation("About to SQL Server Container is running");
+            IPEndPoint sqlServerEndpoint = null;
+            Retry.For(async () => { sqlServerEndpoint = databaseServerContainer.ToHostExposedEndpoint("1433/tcp"); }).Wait();
+
+            // Try opening a connection
+            Int32 maxRetries = 10;
+            Int32 counter = 1;
+
+            //String localhostaddress = Environment.GetEnvironmentVariable("localhostaddress");
+            //if (String.IsNullOrEmpty(localhostaddress))
+            //{
+            //    localhostaddress = "192.168.1.67";
+            //}
+
+            String server = "127.0.0.1";
+            String database = "master";
+            String user = sqlUserName;
+            String password = sqlPassword;
+            String port = sqlServerEndpoint.Port.ToString();
+
+            String connectionString = $"server={server},{port};user id={user}; password={password}; database={database};";
+            logger.LogInformation($"Connection String {connectionString}");
+            SqlConnection connection = new SqlConnection(connectionString);
+            Boolean databaseFound = false;
+            while (counter <= maxRetries)
+            {
+                try
+                {
+                    logger.LogInformation($"Database Connection Attempt {counter}");
+
+                    connection.Open();
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "select * from sys.databases";
+                    SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.Default);
+
+                    logger.LogInformation("Connection Opened");
+
+                    // Check if we need to create the SS database
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            if (dataReader.GetFieldValue<String>(0) == "SubscriptionServiceConfiguration")
+                            {
+                                databaseFound = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    dataReader.Close();
+                    connection.Close();
+                    logger.LogInformation("SQL Server Container Running");
+                    break;
+                }
+                catch (SqlException ex)
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+
+                    logger.LogError(ex);
+                    Thread.Sleep(20000);
+                }
+                finally
+                {
+                    counter++;
+                }
+            }
+
+
+
+            if (databaseFound == false)
+            {
+                // Create the SS database here
+                // Read the SQL File
+                String sqlToExecute = null;
+                String executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                String sqlFileLocation = Path.Combine(executableLocation, "DbScripts");
+                IOrderedEnumerable<String> files = Directory.GetFiles(sqlFileLocation).OrderBy(x => x);
+
+                try
+                {
+                    SqlConnection ssconnection = new SqlConnection(connectionString);
+                    ssconnection.Open();
+                    SqlCommand sscommand = ssconnection.CreateCommand();
+                    sscommand.CommandText = "CREATE DATABASE SubscriptionServiceConfiguration";
+                    sscommand.ExecuteNonQuery();
+
+                    sscommand.CommandText = "USE SubscriptionServiceConfiguration";
+                    sscommand.ExecuteNonQuery();
+
+                    foreach (String file in files)
+                    {
+                        using (StreamReader sr = new StreamReader(file))
+                        {
+                            sqlToExecute = sr.ReadToEnd();
+                        }
+
+                        sscommand.CommandText = sqlToExecute;
+                        sscommand.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+
+                    Console.WriteLine("SS Database Created");
+                }
+                catch (Exception e)
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+            return databaseServerContainer;
         }
 
         public static String GetConnectionString(String databaseName)
