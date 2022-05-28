a21292e69fe9daa49b9bc41b5356dedc730cfefb
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-05-28 07:11:41 +0100

    Added bold bi config

--- EstateAdministrationUI/Common/ConfigurationService.cs
+++ EstateAdministrationUI/Common/ConfigurationService.cs
@@ -1,6 +1,8 @@
 ﻿namespace EstateAdministrationUI.Common
 {
     using System;
+    using System.Collections.Generic;
+    using Shared.Exceptions;
     using Shared.General;
 
     public class ConfigurationService : IConfigurationService
@@ -9,32 +11,58 @@
 
         public String BoldBIAuthorisationUri {
             get {
-                return ConfigurationReader.GetValue("AppSettings", "BoldBIAuthorisationUri");
+                return ConfigurationReader.GetValue("AppSettings:BoldBiConfiguration", "AuthorisationUri");
             }
         }
 
         public String BoldBIEmbedSecret {
             get {
-                return ConfigurationReader.GetValue("AppSettings", "BoldBIEmbedSecret");
+                return ConfigurationReader.GetValue("AppSettings:BoldBiConfiguration", "EmbedSecret");
             }
         }
 
         public String BoldBIRootUri {
             get {
-                return ConfigurationReader.GetValue("AppSettings", "BoldBIRootUri");
+                return ConfigurationReader.GetValue("AppSettings:BoldBiConfiguration", "RootUri");
             }
         }
 
         public String BoldBISiteIdentifier {
             get {
-                return ConfigurationReader.GetValue("AppSettings", "BoldBISiteIdentifier");
+                return ConfigurationReader.GetValue("AppSettings:BoldBiConfiguration", "SiteIdentifier");
             }
         }
 
         public String BoldBIUserEmail {
             get {
-                return ConfigurationReader.GetValue("AppSettings", "BoldBIUserEmail");
+                return ConfigurationReader.GetValue("AppSettings:BoldBiConfiguration", "UserEmail");
             }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public String BoldBIDashboardDataSourceId(String dashboardName) {
+            return this.BoldBIDashboardDetails(dashboardName).dataSourceId;
+        }
+
+        public (String dashboardId, String dataSourceId) BoldBIDashboardDetails(String dashboardName) {
+            Dictionary<String, Dictionary<String, String>> dashboardList =
+                ConfigurationReader.GetValueFromSection<Dictionary<String, Dictionary<String, String>>>("AppSettings:BoldBiConfiguration:Dashboards", null);
+
+            Dictionary<String, String> dashboard = dashboardList.GetValueOrDefault(dashboardName);
+
+            if (dashboard == null) {
+                throw new NotFoundException($"Dashboard with name [{dashboardName}] not found");
+            }
+
+            return (dashboard["Id"], dashboard["DataSourceId"]);
+            ;
+        }
+
+        public String BoldBIDashboardId(String dashboardName) {
+            return this.BoldBIDashboardDetails(dashboardName).dashboardId;
         }
 
         #endregion
