1bfd6309625c9e7b62fa2db1bc60c717990bee1e
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-06-02 17:21:57 +0100

    cert path change

--- EstateAdministrationUI/Program.cs
+++ EstateAdministrationUI/Program.cs
@@ -60,7 +60,7 @@ namespace EstateAdministrationUI
                                                                                                   // Enable support for HTTP1 and HTTP2 (required if you want to host gRPC endpoints)
                                                                                                   listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
                                                                                                   // Configure Kestrel to use a certificate from a local .PFX file for hosting HTTPS
-                                                                                                  listenOptions.UseHttps(Program.LoadCertificate());
+                                                                                                  listenOptions.UseHttps(Program.LoadCertificate(fi.Directory.FullName));
                                                                                               });
                                                                            });
                                                  });
@@ -72,10 +72,10 @@ namespace EstateAdministrationUI
             Program.CreateHostBuilder(args).Build().Run();
         }
 
-        private static X509Certificate2 LoadCertificate()
+        private static X509Certificate2 LoadCertificate(String path)
         {
             //just to ensure that we are picking the right file! little bit of ugly code:
-            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            var files = Directory.GetFiles(path);
             var certificateFile = files.First(name => name.Contains("pfx"));
             Console.WriteLine(certificateFile);
 
